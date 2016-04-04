a668c4f3d0852d94e9f89362cfd9ffde262ca15c
rlskoeser <rebecca.s.koeser@emory.edu>
2016-04-04 17:06:20 -0400

    Make exist-db version environment variables consistent for ci scripts

--- ci/setup-existdb.sh
+++ ci/setup-existdb.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-set -e
-# Is this version of existDB already cached? Meaning, the folder exists
-if [ -d "$EXIST_DB_FOLDER" ]; then
-  echo "Using cached eXist DB instance: ${EXIST_DB_VERSION}."
-  exit 0
-fi
-
 # shell script to download and install a version of exist for testing
-export TARBALL=https://github.com/eXist-db/exist/archive/${EXISTDBVERSION}.tar.gz
+export TARBALL=https://github.com/eXist-db/exist/archive/${EXIST_DB_VERSION}.tar.gz
 mkdir -p ${EXISTDBFOLDER}
 curl -L ${TARBALL} | tar xz -C ${EXISTDBFOLDER} --strip-components=1
 cd ${EXISTDBFOLDER}
