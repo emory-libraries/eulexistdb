fa62b0537cd1832bd36c4a54141c3119ac9d3fb1
rlskoeser <rebecca.s.koeser@emory.edu>
2016-04-04 16:52:29 -0400

    Fix linebreak in continuous integration existdb script

--- ci/setup-existdb.sh
+++ ci/setup-existdb.sh
@@ -1,6 +1,5 @@
 # shell script to download and install a version of exist for testing
-export TARBALL=https://github.com/eXist-db/exist/archive/${EXISTDBVERSION}
-.tar.gz
+export TARBALL=https://github.com/eXist-db/exist/archive/${EXISTDBVERSION}.tar.gz
 mkdir -p ${EXISTDBFOLDER}
 curl -L ${TARBALL} | tar xz -C ${EXISTDBFOLDER} --strip-components=1
 cd ${EXISTDBFOLDER}
