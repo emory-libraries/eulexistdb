145ecefdbcba6bca8d075f0de1e393cd369a074e
rlskoeser <rebecca.s.koeser@emory.edu>
2016-04-04 16:57:48 -0400

    More adjustments to continuous integration setup scripts

--- ci/setup-existdb.sh
+++ ci/setup-existdb.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+# Is this version of existDB already cached? Meaning, the folder exists
+if [ -d "$EXIST_DB_FOLDER" ]; then
+  echo "Using cached eXist DB instance: ${EXIST_DB_VERSION}."
+  exit 0
+fi
+
 # shell script to download and install a version of exist for testing
 export TARBALL=https://github.com/eXist-db/exist/archive/${EXISTDBVERSION}.tar.gz
 mkdir -p ${EXISTDBFOLDER}
--- ci/start-existdb.sh
+++ ci/start-existdb.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
+
 # shell script to start up continunous integration exist instance
 cd ${EXISTDBFOLDER}
 nohup bin/startup.sh &
 sleep 30
+cat nohup.out
 curl http://127.0.0.1:8080/exist
